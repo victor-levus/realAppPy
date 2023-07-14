956745190eec68f6ea14a043ebc64aeb264c909f
victor-levus <95586250+victor-levus@users.noreply.github.com>
2023-07-14 20:33:21 +0100

    make it better

--- database/wsgi.py
+++ database/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'database.settings.dev')
 
 application = get_wsgi_application()
-
-app = application

f1a2c4f3f43bd06115d53e4164ba076ff2cc8d43
victor-levus <95586250+victor-levus@users.noreply.github.com>
2023-07-14 20:46:33 +0100

    added the a;llowed host

--- database/settings/prod.py
+++ database/settings/prod.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['super-mart2.herokuapp.com', 'betcodes-fe.vercel.app/']
+ALLOWED_HOSTS = ["levus-database.herokuapp.com"]
 
 CSRF_TRUSTED_ORIGINS = ['https://super-mart2.herokuapp.com',
                         'https://localhost:3000', 'https://betcodes-fe.vercel.app/']
